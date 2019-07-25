089a0b3bed12e03009fc1207a1093281e65f7285
Onilton Maciel <oniltonmaciel@gmail.com>
2019-07-25 09:57:04 -0300

    Move parse_line to outside of main

--- src/main/scala/ogl.scala
+++ src/main/scala/ogl.scala
@@ -45,43 +45,7 @@ object ogl {
     println("ok")
 
 
-def parse_line(line: String): (Array[(String, String)], String) = {
-    var escapes: mutable.ArrayBuffer[(String, String)] = mutable.ArrayBuffer()
-    var escapes_so_far = ""
-    var clean_line: StringBuilder =  new StringBuilder()
 
-    var index = 0
-    var clean_index = 0
-    var previousWasEscape = false
-    while (index < line.size) {
-        if (line(index) == '\u001b') {
-            val start = line.indexOf("\u001b[", index)
-            if (start != 1) {
-                val end = line.indexOf("m", start)
-                if ((end - start) <= 2) {
-                    if (previousWasEscape) {
-                      escapes_so_far += line.slice(start,end + 1)
-                    } else {
-                      escapes(escapes.size-1) = (escapes(escapes.size -1)._1, line.substring(start, end+1))
-                    }
-                } else {
-                    escapes_so_far += line.slice(start,end + 1)
-                }
-                index = end
-            }
-            previousWasEscape = true
-        } else {
-            //escapes = escapes :+ (escapes_so_far, "")
-            escapes.append((escapes_so_far, ""))
-            escapes_so_far = ""
-            clean_line.append(line(index))
-            clean_index += 1
-            previousWasEscape = false
-        }
-        index = index + 1
-    }
-    return (escapes.toArray, clean_line.toString())
-}
 
 
 val first_non_graph_rgx = """[^*|\\/ _]""".r
@@ -417,4 +381,44 @@ for ((columns, line_number) <- final_.view.zipWithIndex) {
 //# ╿
 //# ┃
 
-}}
+  }
+
+  def parse_line(line: String): (Array[(String, String)], String) = {
+    var escapes: mutable.ArrayBuffer[(String, String)] = mutable.ArrayBuffer()
+    var escapes_so_far = ""
+    var clean_line: StringBuilder =  new StringBuilder()
+
+    var index = 0
+    var clean_index = 0
+    var previousWasEscape = false
+    while (index < line.size) {
+      if (line(index) == '\u001b') {
+        val start = line.indexOf("\u001b[", index)
+        if (start != 1) {
+          val end = line.indexOf("m", start)
+          if ((end - start) <= 2) {
+            if (previousWasEscape) {
+              escapes_so_far += line.slice(start,end + 1)
+            } else {
+              escapes(escapes.size-1) = (escapes(escapes.size -1)._1, line.substring(start, end+1))
+            }
+          } else {
+            escapes_so_far += line.slice(start,end + 1)
+          }
+          index = end
+        }
+        previousWasEscape = true
+      } else {
+        //escapes = escapes :+ (escapes_so_far, "")
+        escapes.append((escapes_so_far, ""))
+        escapes_so_far = ""
+        clean_line.append(line(index))
+        clean_index += 1
+        previousWasEscape = false
+      }
+      index = index + 1
+    }
+
+    (escapes.toArray, clean_line.toString())
+  }
+}
